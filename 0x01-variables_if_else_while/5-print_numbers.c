87e60da526b48ac90ef917d537250abee3932365
Lyleb25 <lyle.booysens@gmail.com>
2022-06-06 16:58:08 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print single number 0 to 10
+ *
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
