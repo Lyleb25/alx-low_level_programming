1c911b8838ac0a59df74ee843d73fed68d5262af
Lyleb25 <lyle.booysens@gmail.com>
2022-06-07 15:05:43 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar.
+ *
+ * Return: 0 (Sucess)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = _putchar()
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
+
