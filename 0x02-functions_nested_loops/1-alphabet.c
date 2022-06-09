71d4ec560d21d4fb76bc487677d7e32fc500c289
Lyleb25 <lyle.booysens@gmail.com>
2022-06-08 18:01:09 -0700

    aalphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,7 +6,7 @@
  * Return: Always 0
  *
  */
-int main(void)
+void print_alphabet(void)
 {
 	int alph;
 
@@ -15,5 +15,5 @@ int main(void)
 		_putchar(alph);
 	}
 	_putchar('\n');
-	return (0);
+	return;
 }
