d44add84f544728c9959a791dd26aec11abf4b1a
Lyleb25 <lyle.booysens@gmail.com>
2022-06-14 16:19:15 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - Entry point
+ *
+ * Description: print string in reverse.
+ * @s: characters to print.
+ *
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int i, count;
+
+	count = 0;
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			count += 1;
+		}
+		for (; count >= 0; count--)
+		{
+			_putchar(s[count]);
+		}
+		_putchar('\n');
+}
