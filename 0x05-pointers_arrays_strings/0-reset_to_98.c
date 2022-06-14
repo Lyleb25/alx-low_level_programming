cd989ef77de12e88d86fa4c14ad6a9e0b92bd6cd
Lyleb25 <lyle.booysens@gmail.com>
2022-06-13 19:22:26 -0700

    Print pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * void reset_to_98 - Entry point
+ *
+ * Description:  pointer to an int as parameter and updates the value to 98
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int n)
+{
+	int n;
+
+	int = 402;
+	{
+		_putchar(n);
+	}
+	*n = 98;
+	{
+		_putchar(n);
+	}
+	
+}
