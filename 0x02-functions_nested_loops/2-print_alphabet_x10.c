7933478796cbf26a3cc6f5eaf3f57f98195f3e13
Lyleb25 <lyle.booysens@gmail.com>
2022-06-21 15:47:05 -0700

    print 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,13 +3,14 @@
 /**
  * print_alphabet_x10 - print alphabet 10x
  *
- * Return: 0 (Success)
+ * Return: void
  */
 void print_alphabet_x10(void)
 {
 	int n = 0, alph;
 
-	while (n <= 10, n++)
+	while (n <= 10)
+		n++;
 		
 	{
 		for (alph = 'a'; alph <= 'z'; alph++)
